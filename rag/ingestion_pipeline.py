080c2a99ec75cf289649add10dbd9bc7d1af5379
subigya-js <developer.subigya@gmail.com>
2026-09-03 13:57:05 +0545

    fix: Update .gitignore to include /data directory and remove unused import in ingestion_pipeline.py

--- rag/ingestion_pipeline.py
+++ rag/ingestion_pipeline.py
@@ -1,6 +1,5 @@
 import os
 import re
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
